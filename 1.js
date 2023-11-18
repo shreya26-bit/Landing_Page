60aa9bba616505d447c5eeb06f026a1b3cfc197e
shreya <shreyasingh222602@gmail.com>
2023-11-18 13:43:23 +0530

    First commit

---
+++ 1.js
@@ -0,0 +1,17 @@
+var connectButton = document.getElementById("connectButton");
+
+connectButton.addEventListener("click", function () {
+  window.location.href = "/";
+});
+
+var connectButton1 = document.getElementById("connectButton1");
+
+connectButton1.addEventListener("click", function () {
+  window.location.href = "/#";
+});
+
+var joinUsButton = document.getElementById("joinUsButton");
+
+joinUsButton.addEventListener("click", function () {
+  window.location.href = "https://wa.me/919746894589";
+});
